806ffd0f48acd2856568f45156b7438e46c24f6b
Amr9876 <amr.aig.2007@gmail.com>
2022-12-27 16:48:24 +0200

    initial commit

---
+++ src/components/Download.jsx
@@ -0,0 +1,35 @@
+import styles from "../styles/global";
+import assets from "../assets";
+
+const Download = () => {
+  return (
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div className="">
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>
+            Download the Source Code
+          </h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>
+            Get the full source code on Github
+          </p>
+        </div>
+        <a
+          href="http://github.com/Amr9876/nft-marketplace"
+          target="_blank"
+          className={styles.btnPrimary}
+        >
+          Source Code
+        </a>
+        <div className={styles.flexCenter}>
+          <img
+            src={assets.scene}
+            alt="download_png"
+            className={styles.fullImg}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Download;
